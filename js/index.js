95770553c7b932ae04a8df674322fde9a433f124
Benjamin Guelle <benjamin.guelle@gmail.com>
2021-03-01 20:27:31 +0100

    add prez section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-document.querySelector('.menu').addEventListener('click', isActiveMenu);
+const btnMenu = document.querySelector('.menu')
+btnMenu.addEventListener('click', isActiveMenu);
 
 function isActiveMenu() {
     const navMobile = document.querySelector('.nav-mobile');
     if ( navMobile.classList.contains('active') ) {
         navMobile.classList.remove('active');
+        btnMenu.classList.remove('active');
     }
     else {
         navMobile.classList.add('active');
+        btnMenu.classList.add('active');
     }
 }
