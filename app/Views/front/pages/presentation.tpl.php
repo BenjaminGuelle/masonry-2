3d0d92e3aa03293a766ff200fa3d147d21f79ec8
Benjamin Guelle <benjamin.guelle@gmail.com>
2021-03-31 12:10:07 +0200

    create presentation page

--- app/Views/front/pages/presentation.tpl.php
+++ app/Views/front/pages/presentation.tpl.php
@@ -1,3 +1,54 @@
 <section class="view_presentation">
-    <span>Hello</span>
+    <div class="society">
+        <article class="society-txt">
+            <h2>
+                <span>
+                    MICHEL Stéphane & fils
+                </span>
+            </h2>
+            <div>
+                <p>
+                    Depuis 2010, notre métier est d'accompagner nos clients, de comprendre leurs projets de réalisation et d'identifier leurs besoins.
+                </p><br>
+                <p>
+                    Spécialisé dans le bâtiment depuis plus de 30 ans, dans le gros oeuvre comme la rénovation, nous garantissons le bon déroulement de vos projets.
+                </p><br>
+                <p>
+                    Fort de notre expérience, nous assurons un diagnostic professionnel et précis pour vous projeter au mieux dans vos projets. Nous saurons également vous guider et vous apporter les conseils dont vous avez besoins.
+                </p><br>
+                <p>
+                    Nos méthodes de travail proféssionelles et exigentent vous garantissent un travail artisanal de qualité et un chantier toujours propre.
+                </p>
+            </div>
+        </article>
+    </div>
+    <div class="society">
+        <article>
+            <h2>
+                <span>
+                    L'équipe
+                </span>
+            </h2>
+            <div>
+                <p>
+                    Notre expérience se transmet de père en fils depuis 7 générations jusqu'à aujourd'hui avec Stéphane MICHEL et son fils Pierre MICHEL.
+                </p><br>
+                <p>
+                    Une équipe exemplaire aux méthodes traditionnelles, ils mettent un point d'honneur à réaliser vos projets jusqu'à atteindre la perfection de leurs réalisations.
+                </p><br>   
+            </div>
+        </article>
+        <div class="team">
+            <div class="worker">
+                <div class="worker-img">
+                    <img src="" alt="">
+                </div>
+                <div>
+                    <span>
+                        
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
