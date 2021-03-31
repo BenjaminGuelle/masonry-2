d73b9bc36956d70c219323b70e69af32c9cfc0f2
Benjamin Guelle <benjamin.guelle@gmail.com>
2021-03-31 18:45:56 +0200

    create presentation page

--- app/Views/front/pages/presentation.tpl.php
+++ app/Views/front/pages/presentation.tpl.php
@@ -1,22 +1,22 @@
 <section class="view_presentation">
     <div class="society">
-        <article class="society-txt">
-            <h2>
+        <article>
+            <h2 class="title">
                 <span>
                     MICHEL Stéphane & fils
                 </span>
             </h2>
             <div>
-                <p>
+                <p class="text">
                     Depuis 2010, notre métier est d'accompagner nos clients, de comprendre leurs projets de réalisation et d'identifier leurs besoins.
                 </p><br>
-                <p>
+                <p class="text">
                     Spécialisé dans le bâtiment depuis plus de 30 ans, dans le gros oeuvre comme la rénovation, nous garantissons le bon déroulement de vos projets.
                 </p><br>
-                <p>
+                <p class="text">
                     Fort de notre expérience, nous assurons un diagnostic professionnel et précis pour vous projeter au mieux dans vos projets. Nous saurons également vous guider et vous apporter les conseils dont vous avez besoins.
                 </p><br>
-                <p>
+                <p class="text">
                     Nos méthodes de travail proféssionelles et exigentent vous garantissent un travail artisanal de qualité et un chantier toujours propre.
                 </p>
             </div>
@@ -24,31 +24,79 @@
     </div>
     <div class="society">
         <article>
-            <h2>
+            <h2 class="title">
                 <span>
                     L'équipe
                 </span>
             </h2>
             <div>
-                <p>
+                <p class="text">
                     Notre expérience se transmet de père en fils depuis 7 générations jusqu'à aujourd'hui avec Stéphane MICHEL et son fils Pierre MICHEL.
                 </p><br>
-                <p>
-                    Une équipe exemplaire aux méthodes traditionnelles, ils mettent un point d'honneur à réaliser vos projets jusqu'à atteindre la perfection de leurs réalisations.
+                <p class="text">
+                    Une équipe exemplaire aux méthodes traditionnelles, nous mettons un point d'honneur à réaliser vos projets jusqu'à atteindre la perfection de leurs réalisations.
                 </p><br>   
             </div>
         </article>
-        <div class="team">
-            <div class="worker">
-                <div class="worker-img">
-                    <img src="" alt="">
-                </div>
-                <div>
-                    <span>
-                        
-                    </span>
-                </div>
+    </div>
+    <div class="team">
+        <!-- Stephane Michel -->
+        <div class="worker">
+            <div class="worker-img">
+                <img src="<?= getPublicAssets('images/presentation/stephane.webp') ?>" alt="Michel Stéphane">
             </div>
+            <div class="worker-description">
+                <h3>
+                    MICHEL Stéphane
+                </h3>
+                <span>
+                    Fondateur & gérant
+                </span>
+            </div>
+        </div>
+        <!-- Pierre Michel -->
+        <div class="worker">
+            <div class="worker-img">
+                <img src="<?= getPublicAssets('images/presentation/man.jpg') ?>" alt="Michel Stéphane">
+            </div>
+            <div class="worker-description">
+                <h3>
+                    MICHEL Pierre
+                </h3>
+                <span>
+                    Employé
+                </span>
+            </div>
+        </div>
+        <!-- Brian -->
+        <div class="worker">
+            <div class="worker-img">
+                <img src="<?= getPublicAssets('images/presentation/man.jpg') ?>" alt="Michel Stéphane">
+            </div>
+            <div class="worker-description">
+                <h3>
+                    MICHEL Brian
+                </h3>
+                <span>
+                    Employé
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="society contact_us">
+        <span>
+            Vous souhaitez nous rencontrer ?
+        </span>
+        <span>
+            Prenez contact avec nous, nous conviendrons d'un rendez-vous.
+        </span>
+        <div class="container_contact">
+            <a href="tel:0641796665" class="btn-contact">
+                <img src="<?= getPublicAssets('images/logoContact/tel.png') ?>" alt="téléphone">
+            </a>
+            <a href="#" class="btn-contact">
+                <img src="<?= getPublicAssets('images/logoContact/letter.png') ?>" alt="mail">
+            </a>
         </div>
     </div>
 </section>
